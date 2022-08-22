752e8d5523561cc3485c9f0af8bd6de8973232aa
kemenykristof <kemenykristof24@gmail.com>
2022-08-22 16:13:00 +0200

    added variables.tf, refactored skeleton to use variables

---
+++ modules/elasticache/main.tf
@@ -0,0 +1,8 @@
+resource "aws_elasticache_cluster" "memcached-cluster" {
+  cluster_id           = var.cluster_id
+  engine               = var.engine
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
+  parameter_group_name = var.parameter_group_name
+  port                 = var.port
+}
